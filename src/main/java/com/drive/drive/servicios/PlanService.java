e555eeb16e75d763a407257854ac07b0b906a9dc
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-12-09 01:43:37 -0600

    solucion

--- src/main/java/com/drive/drive/servicios/PlanService.java
+++ src/main/java/com/drive/drive/servicios/PlanService.java
@@ -2,10 +2,8 @@ package com.drive.drive.servicios;
 
 import com.drive.drive.modelos.Plan;
 
-import oracle.security.crypto.core.Padding.ID;
-
 public interface PlanService {
 
-    public public Plan obtenerPlan(int Plan);
+    public Plan obtenerPlan(int Plan);
 
 }
