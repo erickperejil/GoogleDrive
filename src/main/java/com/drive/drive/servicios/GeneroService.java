3a54a524f580509a35dbaeea5ec0ab8c3778be26
RuthJimenez309 <79241546+RuthJimenez309@users.noreply.github.com>
2023-12-09 01:32:13 -0600

    en genero esta malo   paseme el suyo

--- src/main/java/com/drive/drive/servicios/GeneroService.java
+++ src/main/java/com/drive/drive/servicios/GeneroService.java
@@ -2,13 +2,11 @@ package com.drive.drive.servicios;
 
 import com.drive.drive.modelos.Genero;
 
-<<<<<<< HEAD
 public interface GeneroService {
 
     public Genero crearGenero(Genero genero);
 
-=======
 public interface GeneroService  {
     public Genero crearGenero(Genero genero);
->>>>>>> f9f030726f1c905ac05d012a5932b015d784d0be
+
 }
---
+++ src/main/java/com/drive/drive/servicios/PlanService.java
@@ -0,0 +1,11 @@
+package com.drive.drive.servicios;
+
+import com.drive.drive.modelos.Plan;
+
+import oracle.security.crypto.core.Padding.ID;
+
+public interface PlanService {
+
+    public public Plan obtenerPlan(int Plan);
+
+}
