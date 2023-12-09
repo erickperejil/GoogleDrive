390fe0ac0e048c25d67c82eba616e292909553bf
samuenoc <sreyesz@unah.hn>
2023-12-09 01:44:22 -0600

    fix

--- src/main/java/com/drive/drive/servicios/PlanService.java
+++ src/main/java/com/drive/drive/servicios/PlanService.java
@@ -2,10 +2,9 @@ package com.drive.drive.servicios;
 
 import com.drive.drive.modelos.Plan;
 
-import oracle.security.crypto.core.Padding.ID;
 
 public interface PlanService {
 
-    public public Plan obtenerPlan(int Plan);
+     public Plan obtenerPlan(int Plan);
 
 }
